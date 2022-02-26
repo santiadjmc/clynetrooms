8d32c3b3be3039e30196c8d23ba00df8d441fc9a
= <=>
2022-02-27 00:24:42 +0100

    Added source 'system'

--- logs.js
+++ logs.js
@@ -1,3 +1,3 @@
 const LogManager = require("./managers/LogManager.js");
-const logs = new LogManager(["bot", "web", "websocket"]);
+const logs = new LogManager(["bot", "web", "websocket", "system"]);
 module.exports = logs;
