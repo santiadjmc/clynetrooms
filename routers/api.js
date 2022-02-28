a959090d981144495a0dc52c52cd80399a105a2d
= <=>
2022-02-28 19:32:33 -0300

    Fixed ReferenceError x2

--- routers/api.js
+++ routers/api.js
@@ -26,18 +26,18 @@ router.get("/discord/users", async (req, res) => {
 router.post("/users/pending", async (req, res) => {
     const guild = BotClient.guilds.cache.first();
     const channel = await BotClient.channels.fetch("947973283331047514");
-    const user = await guild.members.fetch(req.body.discordId);
+    const user = await BotClient.users.fetch(req.body.discordId);
     const pendingUsers = await db.query("SELECT * FROM pending_users");
     if (pendingUsers.find(u => u.discordId === user.id)) return res.json({ alreadyIn: true, dmable: null });;
     try {
-        user.user.send("Mensaje de prueba");
+        user.send("Mensaje de prueba");
     }
     catch (err) {
         logs.error("bot", err.message);
         return res.json({ alreadyIn: false, dmable: false });
     }
     res.json({ alreadyIn: false, dmable: true });
-    let mainMsg = await user.user.send(`Se ha recibido una solicitud de inscripcion a Clynet Room por parte de esta cuenta, si no fuiste tu di 'cancelar', de lo contrario di 'continuar'`);
+    let mainMsg = await user.send(`Se ha recibido una solicitud de inscripcion a Clynet Room por parte de esta cuenta, si no fuiste tu di 'cancelar', de lo contrario di 'continuar'`);
     async function getReply() {
         const filter = m => m.author.id === user.id;
         const collected = await mainMsg.channel.awaitMessages({ filter, max: 1 });
@@ -45,10 +45,10 @@ router.post("/users/pending", async (req, res) => {
     }
     let confirmation = await getReply();
     if (confirmation.toLowerCase() === "cancelar") {
-        return await user.user.send("Se ha cancelado la solicitud, gracias por confirmar.");
+        return await user.send("Se ha cancelado la solicitud, gracias por confirmar.");
     }
     else if (confirmation.toLowerCase() !== "cancelar" && confirmation.toLowerCase() !== "continuar") {
-        return await user.user.send("La respuesta no es valida, se ha cancelado el registro de manera automatica.");
+        return await user.send("La respuesta no es valida, se ha cancelado el registro de manera automatica.");
     }
     await db.query("INSERT INTO pending_users SET ?", [{ discordId: user.id }]);
     const row = new MessageActionRow()
